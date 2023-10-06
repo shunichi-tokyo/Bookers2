ce838c3272fc3b4f9fa1313a5033b8275c32e015
EC2 Default User <ec2-user@ip-172-31-47-189.ap-northeast-1.compute.internal>
2023-10-06 13:37:54 +0000

    [Add]10/06

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
 
   def after_sign_in_path_for(resource)
     #flash[:notice] = "Signed in successfully."
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     #flash[:notice] = "Signed out successfully."
-    about_path
+    root_path
   end
 
   protected
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+
 before_action :is_matching_login_user, only: [:edit, :update]
+
   def index
     @users = User.all
     @user = current_user
@@ -20,7 +22,7 @@ before_action :is_matching_login_user, only: [:edit, :update]
     @user = User.find(params[:id])
     if @user.update(user_params)
     flash[:notice] = "You have updated user successfully."
-    redirect_to user_path(@user.id)
+    redirect_to user_path(current_user)
   else
     render :edit
   end
@@ -35,7 +37,7 @@ end
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to users_path
+      redirect_to user_path(current_user)
     end
   end
 
